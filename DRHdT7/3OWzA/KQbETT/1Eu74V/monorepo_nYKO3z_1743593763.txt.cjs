51181d58e40aa445c24737ad4687543d97a28443
KADEroyAl3785 <largess.dishrag_0q@icloud.com>
2025-05-14 14:53:23 +0200

    Improve the file's documentation formatting for better readability

--- DRHdT7/3OWzA/KQbETT/1Eu74V/monorepo_nYKO3z_1743593763.txt.cjs
+++ DRHdT7/3OWzA/KQbETT/1Eu74V/monorepo_nYKO3z_1743593763.txt.cjs
@@ -60,7 +60,6 @@ module.exports = {
 
     'unused-imports/no-unused-imports': 'warn',
     'unused-imports/no-unused-vars': [
-      'warn',
       {
         vars: 'all',
         varsIgnorePattern: '^_',
